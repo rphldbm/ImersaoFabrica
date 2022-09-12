882359a97f390d60583e8dc13169dff039a14b73
Ruben Melo <104650121+gtRuben@users.noreply.github.com>
2022-09-12 17:48:58 -0300

    Add files via upload

---
+++ 01- print, input.py
@@ -0,0 +1,9 @@
+"""
+Em Python, tudo é um objeto.
+"""
+nome=input('Nome: ')
+print('Sua birthday:')
+dia=input('Day: ')
+mes=input('Month: ')
+ano=input('Year: ')
+print('\nOlá,', nome, ', prazer conhece-lo. Voce nasceu dia', dia, 'de', mes, 'de', ano)
---
+++ 02- print format.py
@@ -0,0 +1,5 @@
+"""
+Em Python, tudo é um objeto.
+"""
+nome = input('Nome: ')
+print('é um prazer te conhecer sr. {}'.format(nome))
